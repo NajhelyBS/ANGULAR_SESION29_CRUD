3c064e00d0d91763498c6ca3617b3d2bee6d56fd
NajhelyBS <s5anbandasanchez@gmail.com>
2023-01-30 09:48:06 -0500

    datos se envian correctamente a cloud firestore

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -8,6 +8,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCommonModule } from '@angular/material/core';
 import {MatDialogModule} from '@angular/material/dialog';
 
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -21,6 +26,11 @@ import {MatDialogModule} from '@angular/material/dialog';
     MatButtonModule,
     MatCommonModule,
     MatDialogModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   exports: [
     HeaderComponent,
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,66 @@
-import { Component, Inject } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { ProductoService } from 'src/app/services/producto.service';
+import { Productos } from 'src/app/interfaces/interface';
+import { FormBuilder, FormControl } from '@angular/forms';
+import { FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+
+  public RegistroProducto!: FormGroup;
+
 
   constructor(
-    public dialogRef: MatDialogRef<RegisterComponent>,) { }
+    public dialogRef: MatDialogRef<RegisterComponent>, 
+    private productosservice:ProductoService,
+    private formBuilder:FormBuilder) { }
+
+
+  ngOnInit(): void {
+    this.RegistroProducto = this.formBuilder.group({
+      producto:['',[
+        Validators.required,
+        Validators.minLength(4)
+      ]] ,
+      costo:['',[
+        Validators.required,
+      ]] ,
+      imagen:['',[
+        Validators.required,
+      ]]
+    })
+
+  }
+
+
+  async onSubmit() {
+    this.productosservice.addProducto(this.RegistroProducto.value).then(response => {
+      console.log(response)
+      Swal.fire({
+        icon: 'success',
+        title: 'Registro exitoso',
+        text: 'El producto ha sido añadido a la lista.'
+      });
+    })
+    .catch( error => {
+      console.log(error);
+      Swal.fire({
+        icon: 'error',
+        title: 'Lo lamentamos!',
+        text: 'Ha ocurrido un error al intentar añadir el producto.'
+      });
+    });
+  }
+
+
+
+
 
   onNoClick(): void {
     this.dialogRef.close();
